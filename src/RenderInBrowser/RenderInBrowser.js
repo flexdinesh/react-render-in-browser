baef1187b93dad0cafd009b375c72a9db39ef287
jaabiri salim <jaabiri_salim@hotmail.com>
2018-06-10 04:25:48 +0100

    refactor  RenderBrowser  component and add render props support

--- src/RenderInBrowser/RenderInBrowser.js
+++ src/RenderInBrowser/RenderInBrowser.js
@@ -1,60 +1,25 @@
-import { Component } from 'react';
+
 import PropTypes from 'prop-types';
-import browserCheck from './browser-check';
+import { isSSR } from './utils';
+import { shouldRenderForBrowser } from './handler';
 
-class RenderInBrowser extends Component {
-  constructor(props) {
-    super(props);
-    this.currentBrowser = RenderInBrowser.getCurrentBrowser();
-  }
 
-  /* istanbul ignore next */
-  static getCurrentBrowser () {
-    const browsers = [
-      { name: 'mobile', isCurrentBrowser: browserCheck.isMobile() },
-      { name: 'chrome', isCurrentBrowser: browserCheck.isChrome() },
-      { name: 'firefox', isCurrentBrowser: browserCheck.isFirefox() },
-      { name: 'safari', isCurrentBrowser: browserCheck.isSafari() },
-      { name: 'opera', isCurrentBrowser: browserCheck.isOpera() },
-      { name: 'ie', isCurrentBrowser: browserCheck.isIE() },
-      { name: 'edge', isCurrentBrowser: browserCheck.isEdge() }
-    ];
-
-    const currentBrowser = browsers.filter(browser => browser.isCurrentBrowser === true);
-    if (currentBrowser.length && currentBrowser.length === 1) return currentBrowser[0].name;
-    else undefined;
-  }
-
-  shouldRenderForBrowser() {
-    const { only, except, all, none } = this.props;
-    
-    if (all) return true;
-    else if (none) return false;
-    
-    const browsersInProps = [];
-    /* eslint-disable no-undef */
-    for(let prop in this.props) {
-      if (this.props.hasOwnProperty(prop) && this.props[prop] === true)
-        browsersInProps.push(prop);
+const RenderInBrowser = (props) => {
+  const { children } = props;
+  const shouldRender = !isSSR() && shouldRenderForBrowser(props);
+  if (typeof children === 'function') {
+    const shouldRenderFunc = (args) => {
+      return !isSSR() && shouldRenderForBrowser(args);
     }
-    /* eslint-enable no-undef */
-
-    if (only) return browsersInProps.includes(this.currentBrowser);
-    else if (except) return !browsersInProps.includes(this.currentBrowser);
-    else return browsersInProps.includes(this.currentBrowser);
+    return children(shouldRenderFunc);
   }
-
-  render() {
-    const { children } = this.props;
-    const shouldRender = this.shouldRenderForBrowser();
-    
-    if (shouldRender) {
-      return children;
-    } else {
-      return null;
-    }
+  if (shouldRender) {
+    return children;
+  } else {
+    return null;
   }
 }
+
 
 RenderInBrowser.propTypes = {
   chrome: PropTypes.bool,
@@ -68,7 +33,10 @@ RenderInBrowser.propTypes = {
   only: PropTypes.bool,
   all: PropTypes.bool,
   none: PropTypes.bool,
-  children: PropTypes.node
+  children: PropTypes.oneOfType([
+    PropTypes.func,
+    PropTypes.node,
+  ]),
 };
 
 RenderInBrowser.defaultProps = {
