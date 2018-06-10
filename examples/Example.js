11a3daaaa19a59a303503bb8507cc6baf877b723
jaabiri salim <jaabiri_salim@hotmail.com>
2018-06-10 04:26:16 +0100

    add more example

--- examples/Example.js
+++ examples/Example.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import RenderInBrowser from '../src/index';
+import RenderInBrowser, {withBrowser} from '../src/index';
 
+const HocALll = withBrowser(()=><div>This line will render in all browsers</div>, {
+  all: true
+});
+const HocChromeFirfoxOnly = withBrowser(()=><div>This line will render in chrome firefox only</div>, {
+  chrome: true,
+  firefox: true,
+  only:true
+});
 const Example = () => {
   return (
     <div>
+      <h1> children Component </h1>
       <RenderInBrowser all>
         <div>This line will render in all browsers</div>
       </RenderInBrowser>
@@ -31,6 +40,19 @@ const Example = () => {
       <RenderInBrowser except chrome firefox>
         <div>This line will render in all browsers except chrome firefox</div>
       </RenderInBrowser>
+      <h1>render props component </h1>
+      <RenderInBrowser>
+        {(shouldRenderForBrowser) => {
+          return (<div>
+            {shouldRenderForBrowser({ all: true }) && <div>This line will render in all browsers renderProps</div>}
+            {shouldRenderForBrowser({ none: true }) && <div>This line will not render in any browsers</div>}
+            {shouldRenderForBrowser({ chrome: true, except: true }) && <div>this line will render in all browsers except chrome firefox</div>}
+          </div> ) }
+        }
+      </RenderInBrowser>
+      <h1>Hoc</h1>
+      <HocALll />
+      <HocChromeFirfoxOnly />
     </div>
   );
 };
